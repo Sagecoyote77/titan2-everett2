7e90c5a08c22e03cdf1d587f223fbb7e40e593e1
Sagecoyote77 <35239661+Sagecoyote77@users.noreply.github.com>
2021-02-02 08:55:02 -0500

    servo slowed down

--- titan2-everett-master/src/main/java/frc/robot/RobotContainer.java
+++ titan2-everett-master/src/main/java/frc/robot/RobotContainer.java
@@ -12,10 +12,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
--- titan2-everett-master/src/main/java/frc/robot/commands/CameraCommand.java
+++ titan2-everett-master/src/main/java/frc/robot/commands/CameraCommand.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.Servo;
 
 public class CameraCommand extends CommandBase{
@@ -21,9 +20,16 @@ public class CameraCommand extends CommandBase{
 
     @Override
     public void execute(){
-        while( j0.getPOV() != -1 ){
-            if( j0.getPOV() == 0 ) s0.setAngle( s0.getAngle() + 5 );
-            else if( j0.getPOV() == 180 ) s0.setAngle( s0.getAngle() - 5 );
+        int count = 0;
+        while( count < 30 ){
+            if( count % 5 == 0 ){
+                while( j0.getPOV() != -1 ){
+                    if( j0.getPOV() == 0 ) s0.setAngle( s0.getAngle() + 1 );
+                    else if( j0.getPOV() == 180 ) s0.setAngle( s0.getAngle() - 1 );
+                }
+            }
+            count++;
+            if( count == 30 ) count = 0;
         }
     }
 
