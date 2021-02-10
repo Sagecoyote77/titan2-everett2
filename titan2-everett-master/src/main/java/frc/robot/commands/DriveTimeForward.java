7e852a1f07b5b7a0215d0971d55a6aa5fe55508b
Sagecoyote77 <35239661+Sagecoyote77@users.noreply.github.com>
2021-02-10 15:37:58 -0500

    created drivetimeforward for real

---
+++ titan2-everett-master/src/main/java/frc/robot/commands/DriveTimeForward.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import java.sql.Time;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drive;
+
+public class DriveTimeForward extends CommandBase{
+    private int count = 0;
+  private double time;
+  private Drive drive = RobotContainer.driveTrain;
+
+  public DriveTimeForward( double time ) {
+    // Use requires() here to declare subsystem dependencies
+    addRequirements(drive);
+    this.time = time;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+    count = 0;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  public void execute() {
+      count ++;
+      drive.tankDrive.arcadeDrive( 0.5, 0.5 );
+    }
+
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  public boolean isFinished() {
+    return count >= time*30;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  public void end(boolean interrupted) {
+    drive.tankDrive.arcadeDrive( 0, 0 );
+  }
+
+}
