5737d88459794e4da6c52407e8f105ab1fd714a4
Charlie Mendes <charliemendes@msn.com>
2021-03-04 14:15:50 -0500

    Revert "file backup"
    This reverts commit 26e6f829493df3dd1981a990baab4bb502ecce4c.

--- titan2-everett-master/src/main/java/frc/robot/RobotContainer.java
+++ titan2-everett-master/src/main/java/frc/robot/RobotContainer.java
@@ -59,8 +59,6 @@ public class RobotContainer {
   SendableChooser<CommandBase> winch_chooser = new SendableChooser<>();
 
 
-  //comment for change
-
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
